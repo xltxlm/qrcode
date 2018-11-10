2abbd900538b9ec5e3f8b90a1067227f19c2ff31
xltxlm <xltxlm@qq.com>
2018-11-10 19:44:57 +0800

    加上默认的色彩元素

--- src/qrcode.php
+++ src/qrcode.php
@@ -30,6 +30,34 @@ class qrcode extends qrcode\qrcode_implements
             'eccLevel' => chillerlanQRCode::ECC_M,
         ]);
 
+        // for HTML and SVG
+        $options->moduleValues = [
+            // finder
+            1536 => [0, 63, 255], // dark (true)
+            6 => [255, 255, 255], // light (false), white is the transparency color and is enabled by default
+            // alignment
+            2560 => [0, 63, 255],
+            10 => [255, 255, 255],
+            // timing
+            3072 => [255, 255, 255],
+            12 => [255, 255, 255],
+            // format
+            3584 => [67, 191, 84],
+            14 => [255, 255, 255],
+            // version
+            4096 => [62, 174, 190],
+            16 => [255, 255, 255],
+            // data
+            1024 => [0, 0, 0],
+            4 => [255, 255, 255],
+            // darkmodule
+            512 => [0, 0, 0],
+            // separator
+            8 => [255, 255, 255],
+            // quietzone
+            18 => [255, 255, 255],
+        ];
+
         return (new chillerlanQRCode($options))
             ->render($this->gettext(), $this->getsavefile());
     }
--- test/qrcode/qrcode_146_0.php
+++ test/qrcode/qrcode_146_0.php
@@ -11,8 +11,8 @@ class qrcode_146_0
     public function __invoke()
     {
         $a = (new \xltxlm\qrcode\qrcode)
-            ->settext('abcdd')
-            ->setsavefile(__DIR__ . '/a.png')
+            ->settext('http://www.baidu.com/')
+            ->setsavefile(__DIR__ . '/'.date('YmdHis').'.png')
             ->__invoke();
         \xltxlm\helper\Util::d($a);
         assert(strpos($a, 'data:') !== false);
